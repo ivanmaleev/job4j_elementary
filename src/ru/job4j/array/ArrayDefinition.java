cb15986d5c4b636201f5e90860448e4e7bdc79e3
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-14 15:29:04 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Anton";
+        names[2] = "Alexey";
+        names[3] = "Valeriy";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
